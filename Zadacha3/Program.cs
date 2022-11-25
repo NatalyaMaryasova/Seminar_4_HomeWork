55a1ed7068ea0df329ed0569ea1ef9dd12589cbc
Natalya <nanona@example.com>
2022-11-25 16:49:07 +0700

    Added Zadacha3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая задаёт массив из N элементов и выводит их на экран. Вывод сделать отдельным методом.*/
+
+Console.Write("Введите длинну массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int []array = new int[size];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 1000);
+    Print();
+}
+
+
+void Print()
+{
+    Console.Write(array[i] + " ");
+}
