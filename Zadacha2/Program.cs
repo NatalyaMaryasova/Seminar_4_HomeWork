feb328a8696b0740578d428c85930a6f9a1fc68e
Natalya <nanona@example.com>
2022-11-25 16:48:48 +0700

    Added Zadacha2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе */
+
+int a = ReadInt("Введите число: ");
+int sum = 0;
+while (a > 0)
+{
+    sum = sum + a % 10;
+    a = a /10 ;
+}
+Console.WriteLine(sum);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
